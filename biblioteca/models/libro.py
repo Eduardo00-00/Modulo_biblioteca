e6d39e53239fdd2426a2115cf514457bc7e597bb
Eduardo00-00 <eduardo4lifebreathe@gmail.com>
2025-05-08 18:46:48 +0000

    Se le añadió la vista

--- biblioteca/models/libro.py
+++ biblioteca/models/libro.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Libro(models.Model):
     _name = 'biblioteca.libro'
@@ -7,3 +7,12 @@ class Libro(models.Model):
     name = fields.Char(string='Título', required=True)
     autor = fields.Char(string='Autor')
     fecha_publicacion = fields.Date(string='Fecha de Publicación')
+    dar_el_primer_numeros_para_sumar = fields.Integer(string='Dar primer número para sumar')
+    dar_el_segundo_numeros_para_sumar = fields.Integer(string='Dar segundo número para sumar')
+    resultado = fields.Integer(string='Resultado', compute='_compute_resultado', store=True)
+
+    @api.depends('dar_el_primer_numeros_para_sumar', 'dar_el_segundo_numeros_para_sumar')
+    def _compute_resultado(self):
+        for record in self:
+            record.resultado = record.dar_el_primer_numeros_para_sumar + record.dar_el_segundo_numeros_para_sumar
+
