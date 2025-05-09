bf6612889a92b5f78bb21e73e073becd3045a6d4
Eduardo00-00 <eduardo4lifebreathe@gmail.com>
2025-05-09 16:33:34 +0000

    Errores

--- biblioteca/models/libro.py
+++ biblioteca/models/libro.py
@@ -5,13 +5,7 @@ class Libro(models.Model):
     _description = 'Libro'
 
     name = fields.Char(string='Título', required=True)
-    autor = fields.Char(string='Autor', required=True)
-    editorial = fields.Char(string='Editorial', required=True)
-    fecha_publicacion = fields.Date(string='Fecha de Publicación')
-    dar_el_primer_numeros_para_sumar = fields.Integer(string='Dar primer número para sumar')
-    dar_el_segundo_numeros_para_sumar = fields.Integer(string='Dar segundo número para sumar')
-    resultado = fields.Integer(string='Resultado', compute='_compute_resultado', store=True)
-
+   
     @api.depends('dar_el_primer_numeros_para_sumar', 'dar_el_segundo_numeros_para_sumar')
     def _compute_resultado(self):
         for record in self:
