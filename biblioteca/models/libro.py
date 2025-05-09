5c8ec27784d0dc378fd03b549fd84afd6c5dee4a
Eduardo00-00 <eduardo4lifebreathe@gmail.com>
2025-05-09 17:02:24 +0000

    añadi comentarios

--- biblioteca/models/libro.py
+++ biblioteca/models/libro.py
@@ -13,5 +13,6 @@ class Libro(models.Model):
     @api.depends('dar_el_primer_numeros_para_sumar', 'dar_el_segundo_numeros_para_sumar')
     def _compute_resultado_HT(self):
         for record in self:
+            # Aquí puedes realizar la lógica para calcular el resultado
             record.resultado = record.dar_el_primer_numeros_para_sumar + record.dar_el_segundo_numeros_para_sumar
 
